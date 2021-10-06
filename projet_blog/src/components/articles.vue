68e1ccd5cc5636043015de95a3703843d6bc39c6
Anais <anais.hammouche@edu.itescia.fr>
2021-10-06 15:14:20 +0200

    articles update

--- projet_blog/src/components/articles.vue
+++ projet_blog/src/components/articles.vue
@@ -7,6 +7,8 @@
           <div class="col-md-10 col-lg-8 col-xl-7">
             <h1>{{ articles[0].title }}</h1>
             <p>{{ articles[0].text }}</p>
+            <p> {{ articles[0].date}}</p>
+            <p> Auteur :{{ articles[0].author}}</p>
             Placeholder text by
             <a href="http://spaceipsum.com/">Space Ipsum</a>
             &middot; Images by
@@ -67,6 +69,8 @@ export default {
         {
           title: " What is Lorem Ipsum? ",
           text: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ",
+          author: " Inconnu",
+          date: " 10/10/2021",
         },
       ],
     };
