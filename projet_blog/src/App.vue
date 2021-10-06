096711a72db8ea97536f6cefba2b709a9fa4379f
Anais <anais.hammouche@edu.itescia.fr>
2021-10-06 14:10:05 +0200

    articles templates

--- projet_blog/src/App.vue
+++ projet_blog/src/App.vue
@@ -23,7 +23,7 @@
                 >
               </li>
               <li class="nav-item">
-                <router-link class="nav-link px-lg-3 py-3 py-lg-4" to="/Articles"
+                <router-link class="nav-link px-lg-3 py-3 py-lg-4" to="/articles"
                   >Articles</router-link
                 >
               </li>
