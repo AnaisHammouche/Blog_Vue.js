085de528f6d4f986b4692b112d0bdd41047589a5
AxelDemorest <axel.demorest@gmail.com>
2021-10-06 11:44:08 +0200

    fix problem main.js

--- projet_blog/src/main.js
+++ projet_blog/src/main.js
@@ -8,12 +8,9 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-<<<<<<< HEAD
-=======
 import './assets/css/style.css'
 import './assets/js/scripts.js'
 
->>>>>>> c52f6949092be658856744b84bbfcdb44ee806e3
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
