d324b4eabbc6e606ac59add168101183f1ff15bf
tristanlaine <tristan.laine711@gmail.com>
2021-10-06 15:13:15 +0200

    About components

--- projet_blog/src/views/About.vue
+++ projet_blog/src/views/About.vue
@@ -1,57 +1,15 @@
 <template>
-  <div class="about">
-    <div class="container">
-
-      <h1 class="text-center">Blog réalisé par:</h1>
-      <div class="row text-center p-5">
-        <div class="col-sm-3" style=""><h4>Axel</h4></div>
-        <div class="col-sm-3" style=""><h4>Anaïs</h4></div>
-        <div class="col-sm-3" style=""><h4>Brendan</h4></div>
-        <div class="col-sm-3" style=""><h4>Tristan</h4></div>
-      </div>
-  
-      <h2 class="text-center">Lorem ipsum</h2>
-      <p>
-        Sed ut perspiciatis, unde omnis iste natus error sit voluptatem 
-        accusantium doloremque laudantium, totam rem aperiam eaque ipsa, 
-        quae ab illo inventore veritatis et quasi architecto beatae vitae 
-        dicta sunt, explicabo. <br> 
-        Nemo enim ipsam voluptatem, quia voluptas sit, 
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, 
-        qui ratione voluptatem sequi nesciunt, neque porro quisquam est, 
-        qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, 
-        sed quia non numquam eius modi tempora incidunt, 
-        ut labore et dolore magnam aliquam quaerat voluptatem. <br> 
-        Ut enim ad minima veniam, 
-        quis nostrum exercitationem ullam corporis suscipit laboriosam, 
-        nisi ut aliquid ex ea commodi consequatur? <br> <br> 
-        Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, 
-        quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, 
-        quo voluptas nulla pariatur? <br> <br> 
-        At vero eos et accusamus et iusto odio dignissimos ducimus, 
-        qui blanditiis praesentium voluptatum deleniti atque corrupti, 
-        quos dolores et quas molestias excepturi sint, 
-        obcaecati cupiditate non provident, similique sunt in culpa, 
-        qui officia deserunt mollitia animi, id est laborum et dolorum fuga. <br> 
-        Et harum quidem rerum facilis est et expedita distinctio. <br> 
-        Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, 
-        quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, 
-        omnis dolor repellendus. <br> 
-        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, 
-        ut et voluptates repudiandae sint et molestiae non recusandae. 
-        Itaque earum rerum hic tenetur a sapiente delectus, 
-        ut aut reiciendis voluptatibus maiores alias consequatur 
-        aut perferendis doloribus asperiores repellat. 
-      </p>
-      
-
-    </div>
+  <div>
+  <About/>  
   </div>
 </template>
 
 <script>
+import About from "../components/About.vue"
 export default {
-  name: 'About',
+  components: {
+    About
+  }
 }
 
 </script>
