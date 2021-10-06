dcbd78e8a837b157713bd9ee1ef9c9702db7bf89
AxelDemorest <axel.demorest@gmail.com>
2021-10-06 11:20:54 +0200

    Mise en place du thème bootstrap

--- projet_blog/src/main.js
+++ projet_blog/src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import './assets/css/style.css'
+import './assets/js/scripts.js'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
